c65a2d2a5ec29221237f26daff20af8f5ef2fa70
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-18 18:30:42 +0200

    added content for the left side

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import Header from "./Header";
+import header_img from "../../src/images/header img.png";
 const Hero = () => {
   return (
     <div className="hero">
@@ -10,8 +11,40 @@ const Hero = () => {
           <div></div>
           <span>Najlepsi fachowcy w okolicy</span>
         </div>
+        {/*Hero Heading*/}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Witamy!</span>
+          </div>
+          <div>
+            <span>Firma usługowo - handlowa</span>
+          </div>
+          <div>
+            <span>
+              Zajmujemy się szeroką gamą usług. Jesteśmy pewni, że znajdziesz tu
+              coś dla siebie!
+            </span>
+          </div>
+        </div>
+        {/*figures*/}
+        <div className="figures">
+          <div>
+            <span>+500</span>
+            <span>Zadowolonych klientów</span>
+          </div>
+
+          <div>
+            <span>+200</span>
+            <span>Ukończonych projektów</span>
+          </div>
+        </div>
+        {/*hero buttons*/}
+        <div className="hero-buttons">
+          <buttons className="btn">Dowiedź się więcej</buttons>
+          <buttons className="btn">Skontaktuj się z nami</buttons>
+        </div>
       </div>
-      <div className="right-h">right side</div>
+      <div className="right-h"></div>
     </div>
   );
 };
