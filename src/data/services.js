0e85c58b905777b5f536a736c49550d857b237b6
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-18 14:48:45 +0200

    created react app

---
+++ src/data/services.js
@@ -0,0 +1,44 @@
+export const servicesData = [
+  {
+    icon: <i class="fa-solid fa-plug"></i>,
+    heading: "Elektryka",
+    description:
+      "Zajmujemy się naprawą i wymianą instalacji elektrycznych. Realizujemy także, drobne usługi typu wymiana gniazdek i włączników. Diagnozujemy przyczynę problemów z niestabilnym działaniem istalacji prądowej w gospodarstwach domowych oraz naprawiamy zaistniałą usterkę lub doradzamy możliwe rozwiązania.",
+  },
+  {
+    icon: <i class="fa-solid fa-wrench"></i>,
+    heading: "Hydraulika",
+    description:
+      "Świadczymy usługi hydrauliczne takie jak szybkie usuwanie awarii, ale także montaż kabin prysznicowych, muszli klozetowych, umywalek, wymiana syfonów, udrażnianie odpływów. Montujemy baterie, spłuczki oraz wszelkie inne elementy infrastruktury hydraulicznej. Diagnozujemy przyczyny nieszczelności i usuwamy ich źródło.",
+  },
+  {
+    icon: <i class="fa-solid fa-door-open"></i>,
+    heading: "Okna i drzwi",
+    description:
+      "Usługi z zakresu montażu rolet wewnętrznych, zewnętrznych, a także moskitier. Od ręki realizujemy usługę regulacji drzwi i okien, wymianę zamków i klamek, konserwację zawiasów czy wymianę wewnętrznych skrzydeł drzwiowych.",
+  },
+  {
+    icon: <i class="fa-solid fa-chair"></i>,
+    heading: "Meble i wyposażenie",
+    description:
+      "Oferujemy składanie nowych lub naprawę starych mebli, powieszenie szafki, półki, lustra i innych elementów wyposażenia mieszkania.",
+  },
+  {
+    icon: <i class="fa-solid fa-tv"></i>,
+    heading: "Serwis RTV/AGD",
+    description:
+      "Awaria pralki, zmywarki, telewizora czy innego sprzętu RTV i AGD. Przyjedziemy, zabierzemy, zdiagnozujemy i usuniemy usterkę, a następnie odwieziemy i zamontujemy sprawny sprzęt.",
+  },
+  {
+    icon: <i class="fa-solid fa-bicycle"></i>,
+    heading: "Serwis rowerowy",
+    description:
+      "Serwis rowerowy z dojazdem do klienta. Możliwość wykonania drobnych usterek na miejscu lub zabrania na warsztat w przypadku poważniejszych awarii sprzętu. Wśród naszych usług znajdziesz naprawy bieżące jak i serwis okresowy. Centrowanie oraz zaplatanie kół szprychowych. Przeglądy sezonowe, konserwacja i naprawy. Regulacja hamulców i przekładni. Serwis hydrauliki oraz amortyzatorów. Naprawa oświetlenia.",
+  },
+  {
+    icon: <i class="fa-solid fa-trowel-bricks"></i>,
+    heading: "Prace podwykonawcze firm produkcyjnych",
+    description:
+      "Oferujemy współprace w firmami produkcyjnymi, przyjmujemy stałe jak i jednorazowe zlecenia, usprawniamy wykonywanie powierzonych nam zadań poprzez znakomite rozwiązania i modernizacje zakupionych przez nas sprzętów.",
+  },
+];
