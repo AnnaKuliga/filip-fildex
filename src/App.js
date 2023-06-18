7f9a9ffd27d336615dad24e1dbe783d5a981d621
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-18 17:15:44 +0200

    added logo and menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Hero from "./components/Hero";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../../src/images/logo.png";
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={Logo} alt="" className="logo" />
+      <ul>
+        <li>o nas</li>
+        <li>usługi</li>
+        <li>kontakt</li>
+        <li>
+          <a
+            class="nav-link"
+            aria-current="page"
+            href="https://allegro.pl/uzytkownik/ShareOutlet?strategy=NO_FALLBACK&fbclid=IwAR2pFzbQsHKsi6XnfbGSskmu1qC-UIMMpeQXGpKBpElj-fK__jaHiT57NeM&order=n"
+            target="_blank"
+            title="Allegro Sklep"
+          >
+            <i class="fa-solid fa-cart-shopping"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            class="nav-link"
+            aria-current="page"
+            href="#header-bgd"
+            title="Strona Główna"
+          >
+            <i class="fa-solid fa-house"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
