ef9608af11add191dcc939d14926156fff77f1ff
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-20 18:48:19 +0200

    added services component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <div className="Services" id="services">
+      <div className="services-header">
+        <span>Dowiedz się więcej o firmie</span>
+        <span>FILDEX</span>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
