1a9d309751fc778b3e74e1931c39f594be1ef5cf
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-20 18:54:11 +0200

    added  the heading and styled the section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Services.css";
-
+import { servicesData } from "../../src/data/services";
 const Services = () => {
   return (
     <div className="Services" id="services">
       <div className="services-header">
-        <span>Dowiedz się więcej o firmie</span>
-        <span>FILDEX</span>
+        <span>Dowiedz się więcej o firmie </span>
+        <span className="stroke-text">FILDEX</span>
       </div>
     </div>
   );
