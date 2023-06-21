c15028bd413b23e328a42494cab7d7f4a9301704
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-21 18:25:12 +0200

    added contact component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Info from "./components/Info";
 import Reasons from "./components/Reasons";
@@ -10,6 +11,7 @@ function App() {
       <Services />
       <Reasons />
       <Info />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="Contact" id="contact">
+      <div className="left-c">LEFT</div>
+      <div className="right-c">RIGHT</div>
+    </div>
+  );
+};
+
+export default Contact;
