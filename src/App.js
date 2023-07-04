aeaf2f95e807c749e4c78e47afcd8ab8408f73e3
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-04 19:18:15 +0200

    added join section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Info from "./components/Info";
+import Join from "./components/Join";
 import Reasons from "./components/Reasons";
 import Services from "./components/Services";
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Reasons />
       <Info />
       <Contact />
+      <Join />
       <Footer />
     </div>
   );
