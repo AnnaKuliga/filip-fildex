62c3935683f845cb673d3249f3ce6cc5a1890eb2
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-04 09:31:31 +0200

    Added the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="copyright">ANNA</div>
+      <div className="copyright">
+        <i class="fa-regular fa-copyright"></i> 2023 Coded by
+        <a href="https://annakuliga.com" target="_blank" title="Anna">
+          {" "}
+          Anna Kuliga
+        </a>
+      </div>
     </div>
   );
 };
