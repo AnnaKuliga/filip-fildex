f2ceee286a8cbf112963b55e1e61c51e2d0a204e
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-05 16:25:26 +0200

    added and styled email form

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Join.css";
 
 const Join = () => {
+  const form = useRef();
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
         <hr />
         <div>
-          <span className="stroke-text">nadal nie jesteś </span>
+          <span>nadal</span>
+          <span className="stroke-text"> nie jesteś </span>
           <span>pewny?</span>
         </div>
         <div>
-          <span>zostaw nam wiadomość - </span>
           <span className="stroke-text">oddzwonimy</span>
         </div>
       </div>
-      <div className="right-j"></div>
+      <div className="right-j">
+        <form ref={form} className="email-container">
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Wpisz swój numer telefonu lub e-mail"
+          />
+          <button className="btn btn-j">Wyślij</button>
+        </form>
+      </div>
     </div>
   );
 };
