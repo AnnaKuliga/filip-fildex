ebede5e11d130bdc184e06531866e44f56827560
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-05 15:57:05 +0200

    added and styled the left side of join section

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -5,13 +5,14 @@ const Join = () => {
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
+        <hr />
         <div>
-          <span></span>
-          <span></span>
+          <span className="stroke-text">nadal nie jesteś </span>
+          <span>pewny?</span>
         </div>
         <div>
-          <span></span>
-          <span></span>
+          <span>zostaw nam wiadomość - </span>
+          <span className="stroke-text">oddzwonimy</span>
         </div>
       </div>
       <div className="right-j"></div>
