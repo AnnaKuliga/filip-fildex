6fbd38c6c44a871493b02dd53c1c0a6af0d9554d
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-07-05 16:41:44 +0200

    added email js

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,8 +1,27 @@
 import React, { useRef } from "react";
 import "./Join.css";
+import emailjs from "@emailjs/browser";
 
 const Join = () => {
   const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefaault();
+    emailjs
+      .sendForm(
+        "Your_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
@@ -17,7 +36,7 @@ const Join = () => {
         </div>
       </div>
       <div className="right-j">
-        <form ref={form} className="email-container">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
           <input
             type="email"
             name="user_email"
